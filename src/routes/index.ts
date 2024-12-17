4a5f62d161d11d27079aaa6fb097ba5b5977bd06
haitham-akram <hayhtam14@gmail.com>
2024-12-17 17:19:25 +0200

    Bookmark functionnality

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,8 @@ import authRouter from './auth'
 import petRouter from './pet'
 import petTypeRouter from './petType'
 import followerRouter from './follower'
-import PostsRouter from './User/Posts'
-import CommentsRouter from './User/Comments'
+import PostsRouter from './user/posts'
+import CommentsRouter from './user/comments'
 import BookmarkRouter from './bookmark'
 
 const router = express.Router()
